50ca962240d0696517e51717143ed44cc0e0568e
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-01-13 14:45:59 -0300

    Add files via upload

---
+++ src/test/java/com/Igor/IgorSpringBootProjeto/IgorSpringBootProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Igor.IgorSpringBootProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorSpringBootProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
